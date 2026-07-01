cae212f4863564a9fc613b6c0fa1768ee33c6e85
Raiyan mrf <140499799+raiyanmrf@users.noreply.github.com>
2026-07-01 08:16:38 +0600

    two new params to addBookToLibrary

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 const myLibrary = [];
 
-function Book(name, author, page = null) {
+function Book(id, name, author, page, read) {
   // the constructor...
 
   if (!new.target) {
     throw Error("Invalid calling of this function.");
   }
 
+  this.id = id;
   this.name = name;
   this.author = author;
   this.page = page;
+  this.read = read;
 }
 
-function addBookToLibrary(name, author, page = null) {
+function addBookToLibrary(name, author, page = null, read = false) {
   // take params, create a book then store it in the array
-  const book = new Book(name, author, page);
+  const id = crypto.randomUUID();
+  const book = new Book(id, name, author, page, read);
   myLibrary.push(book);
 }
